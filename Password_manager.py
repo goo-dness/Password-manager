e3d0732f235e18846c69c3401a9c27edc63dac78
Akuba Goodness <goodnessakuba1708@gmail.com>
2025-12-12 12:48:39 +0100

    Implement password manager with encryption features

---
+++ Password_manager.py
@@ -0,0 +1,40 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()                       with open("key.key", "wb") as key_file:               key_file.write(key)
+                                                  
+master_pwd =input("What is the master password?: ")
+key = load_key() + master_pwd.encode()            fer = Fernet(key)
+
+def load_key():                                       file = open("key.key", "rb")                      key = diles.read()
+    file.close()                                      return key
+
+def view_password():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            user, passw = data.split("|")                     print("User:", user,"| Password:", (fer.decrypt (passw.encode()).decode() + "\n"))
+
+
+def add_password():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+    with open('password.txt', 'a') as f:
+        f.write(name + "" +(fer.encrypt (pwd.encod
+e()).decode() +"\n"))
+
+
+
+
+
+while True:
+    mode = input("Do you want to add a new passwor
+d or view an existing one or quit? (add/view/q): "
+)
+    if mode == "q":
+       break
+    elif mode == "view":
+        view()                                        elif mode == "add":
+        add()
+    else:
+        print("Invalid input")
+password_manager.py             40
